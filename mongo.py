41420d2ff25b8524ff4e138eedc6b93ddbde2f17
Sophie Gershon <sophgersh@gmail.com>
2014-11-01 22:22:14 -0400

    adding some mongo stuff that may or may not work

--- mongo.py
+++ mongo.py
@@ -1,19 +1,59 @@
 import pymongo
-from pymongo import MongoClient
+from pymongo import Connection
 
 
 
 #---- run this the first time ------#
 def setup():
-    client = MongoClient()
-    db = client.test_database
-    collection = db.test_collection
-    user = {"fname":"Sophie","lname":"Gershon"}
+    conn = Connection()
+    db = conn['uncommon']
+
+    print db.collection_names()
+    res = db.people.find({},{'_id':False})
+       
+    
     users = db.users
     post_id = users.insert(user)
     users.find_one()
-    
-setup()
 
-def addperson(dict):
+
+def addperson(pdict):
+    db.users.insert(pdict)
+
+def addfield(uname, field, data):
+    p = db.users.find_one(uname)
+    p[field] = data
+    
+    
+#setup()
+
+if __name__ == '__main__':
+    conn = Connection()
+    db = conn.test_database
+    people = db.people
+    dict = {"fname":"Sophie","lname":"Gershon"}
+    people.insert(dict)
+    peeps = people.find({"lname":"Gershon"}) 
+    # >> gives you a list 
+    for p in peeps:
+        print p
+
+"""
+ people = db.people
+ 
+ to insert
+     people.insert(dict)
+        
+ to update:
+     person = people.find_one({"food":"ham"})
+     person["food"] = "eggs"
+     people.save(person)
+
+ to remove:
+     for person in people.find():
+        people.remove(person)
+
+ people.find(dict) --> returns a list of people with the dict qualities 
+"""
+
     
